f1b5ad5cbd8ef907ba3ac02189ee7dcfc11dc934
Kallyan Singha <singhakallyan@gmail.com>
2022-10-27 01:34:10 +0530

    improved some css

---
+++ src/components/Uploads/CsvUploader.js
@@ -0,0 +1,64 @@
+import { Flex, Container,Text, Button } from "@chakra-ui/react";
+import { useState, useEffect,useRef } from "react";
+
+function CsvUploader({height}) {
+  const [File, setFile] = useState(null);
+  const fileRef = useRef()
+  const [FileInside, setFileInside] = useState(false);
+
+  const handleDragOver = (event) => {
+    event.preventDefault();
+    console.log("enter");
+    // if(FileInside===false) setFileInside(true)
+  };
+
+  const handleDrop = (event) => {
+    event.preventDefault();
+    setFile(event.dataTransfer.files[0]);
+    // setFileInside(true)
+  };
+
+  const handleDragLeave = () => {
+    console.log("leave");
+    // if(FileInside===true) setFileInside(false)
+    // FileInside && setFileInside(false)
+  };
+
+  function formatString(fname) {
+    if (fname.length >= 25)
+      return (
+        fname.slice(0, 20) + "..." + fname.slice(fname.length - 5, fname.length)
+      );
+
+    return fname;
+  }
+  return (
+    <Container
+      h={height?height:'50%'}
+      bg="#ffffff1f"
+      m="5px"
+      onDragOver={handleDragOver}
+      onDrop={handleDrop}
+      onDragLeave={handleDragLeave}
+      rounded="20px"
+    >
+      <Flex  alignItems='center' justifyContent='center' h='100%'>
+        {!File ? (
+          <Flex flexDirection='column' justifyContent='space-evenly' w="100%" h='100%' textAlign='center'>
+            <Text> Drag and Drop Your File Here</Text>
+            <span>or,</span>
+            <input type="file" ref={fileRef} onChange={(event)=>setFile(event.target.files[0])} hidden/>
+            <Button w='50%' marginX='auto' onClick={()=>fileRef.current.click()}>Select File</Button>
+          </Flex>
+        ) : (
+          <Flex flexDirection='column' justifyContent='space-evenly' w="100%" h='100%' textAlign='center'>
+            <Text> {formatString(File?.name)}</Text>
+            <Button w='50%' marginX='auto' colorScheme='red' onClick={()=>setFile(null)}>Delete File</Button>
+            </Flex>
+        )}
+      </Flex>
+    </Container>
+  );
+}
+
+export default CsvUploader;
--- src/views/Mailing/index.js
+++ src/views/Mailing/index.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import {Button, Container, Flex ,Textarea } from "@chakra-ui/react";
+import CsvUploader from "components/Uploads/CsvUploader";
 
 const Mailing = () => {
-    return (
-        <div>
-        
-        </div>
-    )
-}
+  return (
+    <Flex
+      flexDirection="column"
+      w="100%"
+      h="100%"
+      pt={{ base: "120px", md: "75px" }}
+    >
+    <Flex
+      flexDirection="row"
+      align="center"
+      justify="center"
+      w="100%"
+      h="100%"
+    >
+      <Container w="50%" h="70vh" p={0} >
+        <Textarea placeholder="Html Here" size="lg" resize="none" height='100%' rounded="20px"/>
+      </Container>
+      <Container w="50%" h="70vh" p={0} >
+        <Flex h="100%" flexDirection="column">
+            <CsvUploader height='100%'/>
+        </Flex>
+      </Container>
+      
+    </Flex>
+    <Button w='20%' mt='20px' mx="25px">Upload File</Button>
+    </Flex>
+  );
+};
 
-export default Mailing
+export default Mailing;

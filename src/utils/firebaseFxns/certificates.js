fca8eb2cbac2ebe121fcf6b8b1cf8080174a97a6
Shreyam Maity <sm8967724231@gmail.com>
2023-02-01 20:07:39 +0000

    feat: firebase functions

---
+++ src/utils/firebaseFxns/certificates.js
@@ -0,0 +1,22 @@
+const { default: CsvUploader } = require("components/Uploads/CsvUploader");
+const { default: ImageUploader } = require("components/Uploads/ImageUploader");
+
+
+
+
+String ImageUploader= async(file) =>{
+    // upload file to firebase storage
+}
+
+List<String> CsvUploader = async(lists) =>{
+    // upload file to firestore
+}
+
+
+bool uploadCertficateAdmin(name, id, lists){
+
+}
+
+
+module.exports = uploadCertficateAdmin;
+
